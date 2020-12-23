b58ac4869cd0549d4b643f2a25267e2c24b9ea5f
Reshmi <reshmitj@gmail.com>
2020-12-23 15:39:42 +0530

    Added

---
+++ WebApiInMVC/MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using MVC.Models;
+using Newtonsoft.Json;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployee> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<mvcEmployee>>().Result;
+            return View(empList);        }
+
+        public ActionResult AddOrEdit(int i = 0)
+        {
+            if (i == 0)
+                return View(new mvcEmployee());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees/" + i.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcEmployee>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcEmployee emp)
+        {
+            if (emp.EmployeeId == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employees", emp).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employees/" + emp.EmployeeId, emp).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete (int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employees/" +id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
